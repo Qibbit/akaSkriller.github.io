e59873effa44d38b241860244949805d00c9602c
Ribbit <davidgates1310@gmail.com>
2024-10-15 15:54:00 -0700

    changed order of files

--- ProductScript.js
+++ ProductScript.js
@@ -1,6 +1,6 @@
-import { cart, addToCart, removeFromCart, minsQuantity } from '../data/cart.js';
-import { products } from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import { cart, addToCart, removeFromCart, minsQuantity } from './cart.js';
+import { products } from './products.js';
+import { formatCurrency } from './money.js';
 // ==============================================================
 let cartImg = document.createElement("img")
 cartImg.setAttribute("src", "./thumbnail/illustration-empty-cart.svg")
