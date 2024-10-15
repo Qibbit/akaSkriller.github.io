fec68f6dd45ee9cf5e8e078c13cb9298dafdbb47
Ribbit <davidgates1310@gmail.com>
2024-10-15 15:43:15 -0700

    changed main folder

--- Product Cart/scripts/utils/money.js
+++
@@ -1,4 +0,0 @@
-export function formatCurrency(PriceCents) {
- return ( PriceCents/100).toFixed(2)
-}
-// this function is to round the money
---
+++ scripts/utils/money.js
@@ -0,0 +1,4 @@
+export function formatCurrency(PriceCents) {
+ return ( PriceCents/100).toFixed(2)
+}
+// this function is to round the money
